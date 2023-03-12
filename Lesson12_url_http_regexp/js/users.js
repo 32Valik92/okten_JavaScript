c2d008ab8620cb1c9ec01e46955c5bee9c3ba18c
32Valik92 <valokstrukalo@gmail.com>
2023-03-12 21:02:04 +0200

    Add name task in users.js

--- Lesson12_url_http_regexp/js/users.js
+++ Lesson12_url_http_regexp/js/users.js
@@ -1,3 +1,9 @@
+// зробити файл users.html
+// з ендпоінту http://jsonplaceholder.typicode.com/users отримати всіх користувачів
+// вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
+// при кліці на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
+// отримана через додатковий запит (https://jsonplaceholder.typicode.com/users/XXX   ХХХ - айді користувача)
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(users => users.json())
     .then(users => {
